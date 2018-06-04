4da26ed9338c41aab90f03258703691946fd3a75
Jeff Chen <thejeffchen@gmail.com>
2018-06-03 22:36:50 -0700

    just make it work

--- 0x02-python_if_else_loops_functions/6-uppercase.py
+++ 0x02-python_if_else_loops_functions/6-uppercase.py
@@ -3,10 +3,10 @@
 def uppercase(string):
     for each_letter in string:
         if ord(each_letter) > 96 and ord(each_letter) < 123:
-            each_letter = ord(each_letter) - 32
+            each_letter = chr(ord(each_letter) - 32)
         else:
-            each_letter = ord(each_letter)
-        print(chr(each_letter), end="")
+            each_letter = chr(ord(each_letter))
+        print("{}".format(each_letter), end="")
     print("")
 
 
